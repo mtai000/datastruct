e0e16116e9c9e9ae376a1a87614d374f1271c100
mtai000 <605836858@qq.com>
2018-05-08 14:04:11 +0000

    Add InsertList and GetElem

--- chapter2/list.c
+++ chapter2/list.c
@@ -35,6 +35,57 @@ Status DestroyList(SqList *L)
         return ERROR;
     return OK;
 }
+
+Status ClearList(SqList *L)
+{
+    L->length = 0;
+    return OK;
+}
+
+Status ListEmpty(SqList L)
+{
+    return L.length == 0? TRUE : FALSE;
+}
+
+int ListLength(SqList L)
+{
+    return L.length;
+}
+
+Status GetElem(SqList L , int i ,int *e)
+{
+    if(i < 1 || i > ListLength(L))
+    {
+        printf("Get invalid value : i = %d\n", i);
+        return ERROR;
+    }
+    *e = L.elem[i-1];
+    return OK;
+}
+
+Status ListInsert(SqList *L , int i, int e)
+{
+    if(i < 1 || i > ListLength(*L)+1)
+    {
+        printf("Insert invalid value : i = %d\n", i);
+        return ERROR;
+    }
+    if(ListLength(*L) >= L->listsize)
+    {
+        int *newbase = (Elem*)realloc(L->elem, (L->listsize + LISTINCREMENT) * sizeof (Elem));
+        if(!newbase) return ERROR;
+        L->elem = newbase;
+        L->listsize += LISTINCREMENT;
+    }
+
+    int *q = &(L->elem[i - 1]);
+    for( int *p = &(L->elem[L->length - 1]); p >= q; --p) *(p+1) = *p;
+    *q = e;
+    ++ (L->length);
+      
+    return OK;
+}
+
 int main(void)
 {
     SqList L;
@@ -46,6 +97,22 @@ int main(void)
         printf("Destroy List success\n");
     else
         printf("Destroy List fail\n");
-
+    if(ClearList(&L))
+        printf("Clear List success\n");
+    else
+        printf("Clear List fail\n");
+    if(ListEmpty(L))
+        printf("List is Empty\n");
+    else
+        printf("List is not Empty\n");
+    InitList(&L);
+    ListInsert(&L,1,3);
+    ListInsert(&L,2,1);
+    ListInsert(&L,1,6);
+    int e = 0;
+    GetElem(L,2,&e);
+    printf("%d\n",e);
+    DestroyList(&L);
     return 0;
 }
+
